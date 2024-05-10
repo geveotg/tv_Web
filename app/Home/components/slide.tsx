794c6273861aa79f111574ab04f6a0421f234411
MariMuradyan <muradyan2005m@gmail.com>
2024-05-10 18:05:00 +0400

    refresh slide

--- app/Home/components/slide.tsx
+++ app/Home/components/slide.tsx
@@ -1,25 +1,21 @@
 import React from "react";
 import { Carousel } from "antd";
 import classes from "../style/slide.module.scss";
+import { dataSlide } from "@/app/datas/dataSlide";
 const Slide: React.FC = () => {
+
+    const getIcons= dataSlide.map((e)=>{
+        return(
+            <div key={e.id}>
+                <img className={classes["slide_images"]} src={e.image} alt="" />
+            </div>
+        )
+    })
+
     return (
         <div className={classes["silide_container"]}>
             <Carousel autoplay arrows infinite={false}>
-                <div>
-                    <img className={classes["slide_images"]} src="/images/carusel/1.jpg" alt="" />
-                </div>
-                <div>
-                    <img className={classes["slide_images"]} src="/images/carusel/2.jpg" alt="" />
-                </div>
-                <div>
-                    <img className={classes["slide_images"]} src="/images/carusel/3.jpg" alt="" />
-                </div>
-                <div>
-                    <img className={classes["slide_images"]} src="/images/carusel/4.jpg" alt="" />
-                </div>
-                <div>
-                    <img className={classes["slide_images"]} src="/images/carusel/5.jpg" alt="" />
-                </div>
+               {getIcons}
             </Carousel>
         </div>
     );
